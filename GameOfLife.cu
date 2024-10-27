ae9e78ffb84b6c7a19ebe67bf121310f8506efef
Yash Saxena <yash.saxena@gmail.com>
2024-10-26 23:16:32 -0700

    initial commit

---
+++ GameOfLife.cu
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <cuda_runtime.h>
+
+#define BOARD_HEIGHT 9;
+#define BOARD_WIDTH  9;
+
+int main(int argc, char** argv) {
+    printf("Hello, world!\n");
+}
